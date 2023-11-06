a74e23c15abc28c75fa76a82a6a80d8ffafaf8db
Brian Takita <brian.takita@gmail.com>
2023-11-06 12:50:52 -0500

    dependencies:
    	∋ @ctx-core/vanjs
    	update dependencies

--- src/ctx/browser__ctx.ts
+++ src/ctx/browser__ctx.ts
@@ -1,12 +1,13 @@
-import { blog__ctx__new, van__set } from '@btakita/domain--all--blog'
+import { blog__ctx__new } from '@btakita/domain--all--blog'
 import { globalThis__prop__ensure } from '@ctx-core/object'
+import { props_clean_van__new, van__set } from '@ctx-core/vanjs'
 import van from 'vanjs-core'
 export const browser__ctx__ensure = globalThis__prop__ensure(
 	'browser__ctx__ensure',
 	()=>()=>globalThis__prop__ensure(
 		'browser__ctx', ()=>{
 			const ctx = browser__ctx__new()
-			van__set(ctx, van)
+			van__set(ctx, props_clean_van__new(van))
 			return ctx
 		}))
 function browser__ctx__new() {
